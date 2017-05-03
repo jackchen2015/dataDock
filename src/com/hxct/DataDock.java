77844ee5829225d906c4cdcd3a8898f72baa3aaf
jackchen2015 <chenw_2002@163.com>
2017-05-03 10:59:11 +0800

    初始导入

---
+++ src/com/hxct/DataDock.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2015 Hongxin Telecommunication Technologies Co, Ltd.,
+ * Wuhan, Hubei, China. All rights reserved.
+ */
+
+package com.hxct;
+
+/**
+ *
+ * @author chenwei
+ * Created on 2017-5-3, 10:57:11
+ */
+public class DataDock
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+
+}
